810ecb9f614bdb0e607dbb28f39533c5ba69174e
nnastaran83 <nnastaran83@gmail.com>
2022-12-02 15:47:11 +0200

    created post list

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,5 +2,5 @@ import jsonplaceholder from "../apis/jsonplaceholder";
 
 export const fetchPosts = () => async disptch => {
     const response = await jsonplaceholder.get('/posts');
-    disptch({type: 'FETCH_POSTS', payload: response});
+    disptch({type: 'FETCH_POSTS', payload: response.data});
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,7 @@
 import { combineReducers } from "redux";
-
-
+import postReducer from "./postReducer";
 
 
 export default combineReducers({
-    dummy: () => 'dummy'
+    posts: postReducer 
 });
