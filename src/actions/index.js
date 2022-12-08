59b8c1584b2fd69e8297943784a1b0dd96cd13b3
nnastaran83 <nnastaran83@gmail.com>
2022-12-08 11:21:41 +0200

    less fetching

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,11 @@
+import _ from "lodash";
 import jsonplaceholder from "../apis/jsonplaceholder";
+
+export const fetchPostsAndUsers =() => async (dispatch,getState) => {
+    await dispatch(fetchPosts());
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+};
 
 export const fetchPosts = () => async disptch => {
     const response = await jsonplaceholder.get('/posts');
@@ -6,7 +13,19 @@ export const fetchPosts = () => async disptch => {
 };
 
 
-export const fetchUser = (id) => async dispatch => {
+export const fetchUser = id => async dispatch => {
     const response = await jsonplaceholder.get(`users/${id}`);
     dispatch({type: 'FETCH_USER', payload: response.data});
 };
+
+
+
+
+//export const fetchUser = id => dispatch => {
+//   _fetchUser(id, dispatch)
+//};
+//
+//const _fetchUser = _.memoize(async (id, dispatch) => {
+//    const response = await jsonplaceholder.get(`users/${id}`);
+//    dispatch({type: 'FETCH_USER', payload: response.data});
+//});
